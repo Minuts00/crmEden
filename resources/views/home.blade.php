d7e605977142a93ce030f9efddb968e84f5ca552
Minuts00 <alessiom181@gmail.com>
2025-07-18 10:35:56 +0200

    inizio front

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,16 +16,37 @@
                 <x-layout>
 
 </x-layout>
-<div class="container" id="catalog">
-    <div class="row">
-        <div class="col-2">
-
+<div class="container-fluid" id="catalog">
+    <div class="row justify-content-evenly">
+        <div class="col-3">
+            <div class="card glow">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
         </div>
-        <div class="col-8">
-            <!-- cards js -->
         </div>
-        <div class="col-2">
-            
+        <div class="col-3">             
+            <div class="card glow">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        </div>
+        <div class="col-3">
+            <div class="card glow">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
         </div>
     </div>
 </div>
